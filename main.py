6753b76eb1819591a6622d080e3ffb818d1b24d2
Annie <annielaloca99@gmail.com>
2021-02-18 23:58:37 +0100

    Por favor

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 from twitchio.ext import commands
-
-
-
-with open("config.secret","r") as fp:
-    lines = fp.readlines()
-    config = {}
-    for line in lines:
-        partes = line.replace("\n","").split("=")
-    config[partes[0]] = partes[1]
-
-
-bot = commands.Bot(
-    irc_token=config['TOKEN'],
-    client_id =config['CLIENT_ID'],
-    nick=config['BOT_NICK'],
-    prefix=config['BOT_PREFIX'],
-    initial_channels=[config['CHANNEL']]
-)
-
-@bot.event
-async def event_ready():
-    print(f"{data['BOT_NICK']} online!")
-    ws = bot._ws  # solo se necesita en event_ready
-    await ws.send_privmsg(data['CHANNEL'], f"/Ha llegado")
-@bot.event
-async def event_message(ctx):
-    'Runs every time a message is sent in chat.'
-    print(ctx.author.name)
-
-bot.run()
-
-@bot.command(name='test')
-async def test(ctx):
-    print(ctx)
-    await ctx.send(f'Test Pasado! Gracias {ctx.author.name}')
-#Cambiamos el manejador de mensajes, para que maneje los comandos
-@bot.event
-async def event_message(ctx):
-    'Runs every time a message is sent in chat.'
-    await bot.handle_commands(ctx)
+class Bot(commands.Bot):
+    def __init__(self):
+        nick = ""
+        chanel = ""
+        with open("config.secret","r") as fp:
+            lines = fp.readlines()
+            config = {}
+            for line in lines:
+                partes = line.replace("\n","").split("=")
+                config[partes[0]] = partes[1]
+            super().__init__(
+                irc_token=config['TOKEN'],
+                client_id =config['CLIENT_ID'],
+                nick=config['BOT_NICK'],
+                prefix=config['BOT_PREFIX'],
+                initial_channels=[config['CHANNEL']]
+            )
+        self.nick = config['BOT_NICK']
+        self.channel = config['CHANNEL']
+            
+    
+    async def event_ready(self):
+        print(f'Listo! | {self.nick}')
+        ws = self._ws 
+        await ws.send_privmsg(self.channel, f"/me has landed!")
+    async def event_message(self, message):
+        print(message.content)
+        await self.handle_commands(message)
+    # Decorador para los comandos
+    @commands.command(name='saludo')
+    async def saludo(self, ctx):
+        await ctx.send(f'Hola {ctx.author.name}!')
