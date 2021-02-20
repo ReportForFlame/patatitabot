426e03ff44b5772e8cb8b70eebd5ac0be82b24d3
ReportForFlame <36738129+ReportForFlame@users.noreply.github.com>
2021-02-21 00:20:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Bot(commands.Bot):
         if patron1.search(message.content.lower()):
             await message.channel.send(f"¡Hola @{message.author.name}, bienvenido/a al stream!")
         elif patron2.search(message.content.lower()):
-            await message.send(f'Consigue un cofre gratis de mi colección de Streamloots https://www.streamloots.com/reportforflame?couponCode=YIT26')
+            await message.channel.send(f'Consigue un cofre gratis de mi colección de Streamloots https://www.streamloots.com/reportforflame?couponCode=YIT26')
 
     '''timeout = 30.0 # Segundos del timer
 
