599a704a556950d9ed242a3a54d45e9b48a20281
ReportForFlame <36738129+ReportForFlame@users.noreply.github.com>
2021-03-01 20:33:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Bot(commands.Bot):
 
         if patron1.search(message.content.lower()):
             await message.channel.send(f"¡Hola @{message.author.name}, bienvenido/a al stream!")
-            await message.channel.send(f"/commercial 60")
+            await message.channel.send(/commercial 60)
         elif patron2.search(message.content.lower()):
             await message.channel.send(f'¿He leido cofre? Puedes conseguir un cofre gratis de la colección de @ReportForFlame en Streamloots https://www.streamloots.com/reportforflame?couponCode=YIT26')
     
