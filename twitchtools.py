0f236a3cde091f56b5cfb98cc4bc8c57630f6159
ReportForFlame <36738129+ReportForFlame@users.noreply.github.com>
2021-04-16 23:37:57 +0200

    Update twitchtools.py

--- twitchtools.py
+++ twitchtools.py
@@ -127,6 +127,7 @@ def streamLive():
     data = json.loads(resultsStr)
     print(f'Sending streamGame results: {data}')
     live = data.get('data')[0]
+    print(live)
     live = live.get('type')
     print(live)
     return live
