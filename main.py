2d79da370aacf1094d9652990bce2dc37d3e52a7
ReportForFlame <36738129+ReportForFlame@users.noreply.github.com>
2021-04-17 01:23:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ class Bot(commands.Bot):
     async def follow(self, ctx):
         fecha = followSince(ctx.author.id)
         today = date.today()
-        past_date = datetime.strptime(fecha, '%Y-%m-%dT%H:%M:%SZ')
+        past_date = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
         print(past_date)
         days = (today - past_date).days
         await ctx.send(days)
