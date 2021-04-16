0184de7e3e40d8c57999d28ab8230d604990980b
ReportForFlame <36738129+ReportForFlame@users.noreply.github.com>
2021-04-16 23:32:48 +0200

    Update twitchtools.py

--- twitchtools.py
+++ twitchtools.py
@@ -109,7 +109,7 @@ def setGame(name):
     print(f'Sending setGame results: {results}')
 
 def streamLive():
-    url = 'https://api.twitch.tv/helix/streams?user_login=reportforflame'
+    url = 'https://api.twitch.tv/helix/users?id=83232866'
 
     headers = {
             'Accept': 'application/vnd.twitchtv.v5+json',
